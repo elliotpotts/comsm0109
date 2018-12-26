a3a229b63b8582033b21da6ea7264128c46244da
Elliot Potts <elliotpotts@gmail.com>
2018-12-26 20:40:07 +0000

    Moved forwarding into lsq file

--- src/execution_unit.cpp
+++ src/execution_unit.cpp
@@ -4,7 +4,6 @@
 #include <unordered_set>
 #include <iterator>
 #include <variant>
-#include <boost/iterator/iterator_adaptor.hpp>
 
 void sim::alu::start() {
     if (executing) return;
@@ -28,41 +27,6 @@ void sim::alu::finish() {
     if (ticks_left <= 0) {
         executing->execute();
         executing.reset();
-    }
-}
-
-void sim::forward_stores() {
-    for (auto ld_it = sim::lsq.begin(); ld_it != sim::lsq.end(); ld_it++) {
-        auto ld_ptr = std::get_if<sim::load>(&*ld_it);
-        if (ld_ptr && ld_ptr->addr && !ld_ptr->data) {
-            // Find a store with a matching address
-            bool potential_conflict = false;
-            auto fwdng_it = std::find_if (
-                boost::make_reverse_iterator(ld_it),
-                sim::lsq.rend(),
-                [&](const load_store& ls) {
-                    return std::visit( match {
-                        [&](const store& st) {
-                            // An unknown store address poses a conflict as
-                            // any loads occurring aftwards could load from
-                            // it.
-                            if (!st.addr) potential_conflict = true;
-                            return !potential_conflict
-                                && st.addr
-                                && *st.addr == *ld_ptr->addr
-                                && st.data;
-                        },
-                        [&](const load&) { return false; }
-                    }, ls);
-                }
-            );
-            if (fwdng_it != sim::lsq.rend()) {
-                if (ld_ptr->loader) {
-                    ld_ptr->loader->cancel();
-                }
-                ld_ptr->data.fulfil(*std::get<store>(*fwdng_it).data);
-            }
-        }
     }
 }
 
