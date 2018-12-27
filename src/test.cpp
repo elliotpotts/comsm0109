d9f3a8f0d2506e808f93ad2186efb40ed6422c3f
Elliot Potts <elliotpotts@gmail.com>
2018-12-27 23:12:18 +0000

    Added vector test

--- src/test.cpp
+++ src/test.cpp
@@ -149,3 +149,56 @@ TEST_CASE ("Sum array") {
         }
     }
 }
+
+TEST_CASE ("Vector sum") {
+    std::vector<memcell> image;
+    int length = 50;
+    int lhs_base = image.size();
+    image.insert(image.end(), {
+        -45, 8, -62, 17, -81, -89, 75, 12, 60, -30, -48, 58, 71, 89, 75, -60, -35, -79, -21,
+        -75, -14, 4, 64, -82, -12, 4, 100, -47, -51, 61, 86, -17, 36, 3, 80, 33, 67, -46,
+        50, -72, -13, -51, 83, 73, -60, -16, 78, -98, -52, 47
+    });
+    int rhs_base = image.size();
+    image.insert(image.end(), {
+        43, -98, 18, 87, 40, -65, 73, -38, -46, -34, -38, -24, 67, 65, -12, -49, -48, 24, 58,
+        0, -70, 25, -42, 23, 0, -14, -66, -43, 97, -92, 31, 0, 89, -33, -66, 97, 16, 52, 44,
+        63, 41, -66, -94, 89, 89, 13, 54, 42, -11, -37
+    });
+    int res_base = image.size();
+    image.insert(image.end(), 100, -1);
+    int start = image.size();
+    const areg offset = areg::g0;
+    const areg addr = areg::g1;
+    const areg lhs = areg::g2;
+    const areg rhs = areg::g3;
+    const areg sum = areg::g4;
+    const areg ord = areg::g5;
+    auto m = std::back_inserter(image);
+    m++ = add {offset, lhs_base, addr};
+    m++ = ldw {addr, lhs};
+    m++ = add {offset, rhs_base, addr};
+    m++ = ldw {addr, rhs};
+    m++ = add {lhs, rhs, sum};
+    m++ = add {offset, res_base, addr};
+    m++ = stw {sum, addr};
+    m++ = add {offset, 1, offset};
+    m++ = cmp {offset, length, ord};
+    m++ = jeq {ord, -1, -9};
+    m++ = halt {};
+
+    const std::vector<word> expected = {-2, -90, -44, 104, -41, -154, 148, -26, 14, -64, -86, 34,
+    138, 154, 63, -109, -83, -55, 37, -75, -84, 29, 22, -59, -12, -10, 34, -90, 46, -31, 117, -17,
+    125, -30, 14, 130, 83, 6, 94, -9, 28, -117, -11, 162, 29, -3, 132, -56, -63, 10};
+    for (auto cfg : cfgs) {
+        WHEN (cfg.name) {
+            sim::reset (cfg, image, start);
+            sim::run_until_halt();
+            std::vector<word> calculated;
+            for(unsigned i = 0; i < length; i++) {
+                calculated.push_back(std::get<word>(main_memory[res_base + i]));
+            };
+            REQUIRE ( calculated == expected );
+        }
+    }
+}
